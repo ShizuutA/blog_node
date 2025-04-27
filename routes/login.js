1b41ec2137de54fb3ef3080d947337d60e5c9452
ShizuutA <romainardoise84@gmail.com>
2025-04-27 12:40:49 +0200

    update styles for consistency across login and registration pages

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'cielpass',
+    password: 'root',
     database: 'Blog_Romain'
 });
 
